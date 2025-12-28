01a163ed194f94c36a371ac07f22947ab7c841c2
Diego Fernandes Branco <diego.f.b.code@gmail.com>
2025-12-28 16:53:21 +0100

    added struct examples

--- raspberryos/src/main.rs
+++ raspberryos/src/main.rs
@@ -1,3 +1,9 @@
+mod test;
+mod test1 {
+    pub mod testnest;
+}
 fn main() {
     println!("Hello, world!");
+    test::funct();
+    test1::testnest::test();
 }
---
+++ raspberryos/src/test1/testnest/mod.rs
@@ -0,0 +1,3 @@
+pub fn test() {
+    println!("I went through!");
+}
